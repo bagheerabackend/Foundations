6007fce83c6c254a646608fa2532727f917d1799
Bagheera Backend <bagheerabackend@gmail.com>
2025-12-08 11:44:37 +0530

    Refactor header layout for improved alignment and structure

--- app/components/home/header.vue
+++ app/components/home/header.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="flex items-center justify-between">
-        <div class="inline-block">
-            <h1 class="font-[alfa] text-[2em] md:text-[3em] text-[#FB8500]">GOLD STAR</h1>
-            <h2 class="mt-[-0.8em] text-[1em] md:text-[1.5em] text-black/60 text-center font-light">Foundations & Charities</h2>
+        <div class="flex items-center ">
+            <img src="/icon-192x192.png" alt="Gold Star Foundations Icon" srcset="" class="h-[3em] md:h-[5em] mr-[0.5em]"/>
+            <div class="inline-block"> 
+                <h1 class="font-[alfa] text-[2em] md:text-[3em] text-[#FB8500]">GOLD STAR</h1>
+                <h2 class="mt-[-0.8em] text-[1em] md:text-[1.5em] text-black/60 text-center font-light">Foundations & Charities</h2>
+            </div>
         </div>
         <div>
             <NuxtLink to="/#donate" class="text-[1em] md:text-[1.25em] px-[2em] py-[0.8em] rounded-[0.5em] hover:bg-[#FB8500] hover:text-[#fff] transition-colors duration-500 ease-[cubic-bezier(.4,0,.2,1)] ">Donate</NuxtLink>
